4fa1072d27bbc187da52901e26fd8dc36ca18bc1
unknown <aelatrouz@gmail.com>
2022-05-17 19:31:02 +0100

    feature-[BACKEND] : add api call for products by categoryId

--- 02-backend/ecommerce-spring-boot/src/main/java/com/caocao/me/dao/ProductRepository.java
+++ 02-backend/ecommerce-spring-boot/src/main/java/com/caocao/me/dao/ProductRepository.java
@@ -1,11 +1,19 @@
 package com.caocao.me.dao;
 
 import com.caocao.me.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RepositoryRestResource
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+    // we can call this method using a url like this : /api/products/search/findByCategoryId?id=1
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
