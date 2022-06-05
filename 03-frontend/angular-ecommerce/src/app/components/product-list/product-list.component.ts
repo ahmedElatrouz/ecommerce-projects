df1a514e640fa268106716f06a6ee98259c004de
unknown <aelatrouz@gmail.com>
2022-06-05 18:22:46 +0100

    feature - [FRONTEND] : add cart service

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -23,6 +25,7 @@ export class ProductListComponent implements OnInit {
 
 
   constructor(private productService: ProductService,
+              private cartService: CartService,
               private route: ActivatedRoute) { }
   
 
@@ -100,6 +103,8 @@ export class ProductListComponent implements OnInit {
   }
 
   addToCart(product: Product) {
-      //TODO .. add to cart
+      const theCartItem = new CartItem(product);
+      this.cartService.addToCart(theCartItem);
+
   }
 }
