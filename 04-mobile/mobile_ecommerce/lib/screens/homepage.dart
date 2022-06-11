279de029dddbfffa5518e8d886181d3045bb0276
aelatrouz <aelatrouz@sqli.com>
2022-06-11 23:55:58 +0100

    feature - [MOBILE] : style appBar in homepage

--- 04-mobile/mobile_ecommerce/lib/screens/homepage.dart
+++ 04-mobile/mobile_ecommerce/lib/screens/homepage.dart
@@ -9,12 +9,15 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("HomePage"),
+        title: Text(
+          "HomePage",
+          style: TextStyle(color: Colors.black),),
         centerTitle: true,
         elevation: 0.0,
+        backgroundColor: Colors.grey[100],
         actions: <Widget>[
           IconButton(onPressed: (){}, icon: Icon(Icons.notifications),color: Colors.black,),
-          IconButton(onPressed: (){}, icon: Icon(Icons.send)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.send),color: Colors.black,),
 
         ],
       ),
